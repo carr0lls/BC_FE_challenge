86854660c6626c1f65bb5a8d21052dbe1ad10bca
Carroll Yu <carrollcyu@gmail.com>
2017-07-08 13:48:24 -0700

    filters

--- public/script.js
+++ public/script.js
@@ -2,17 +2,21 @@
 	
 	const API_BASE_URL = `http://localhost:3000`;
 	const SEARCH_TIMEOUT_DELAY = 1000;
-	
+	// Search elements
 	const search = document.getElementById("search");
+	// List view elements
 	const companyList = document.getElementById("company-list");
-
+	// Detailed view elements
 	const detailedCompany = {};
 	detailedCompany['name'] = document.querySelector("#company-info .name");
 	detailedCompany['phone'] = document.querySelector("#company-info .phone");
 	detailedCompany['website'] = document.querySelector("#company-info .website");
 	detailedCompany['avatar'] = document.querySelector("#company-info .avatar img");
 	detailedCompany['laborType'] = document.querySelector("#company-info .labor-type");
-
+	// Filter elements
+	const filters = document.querySelectorAll(".filter");
+	let filterString = ``;
+	// Pagination elements
 	const previousPage = document.getElementById("previous-page");
 	const nextPage = document.getElementById("next-page");
 	const currentPage = document.getElementById("current-page");
@@ -57,32 +61,49 @@
 	}
 
 	function bindEventListeners() {
-		let searchTimestamp, detailedCompanyInfo;
-
+		// Search event listener
+		let searchTimestamp;
 		search.addEventListener("keyup", function(e) {
 			if (searchTimestamp)
 				clearTimeout(searchTimestamp);
 
 			startingEntry = 0;
-			searchTimestamp = setTimeout(searchCompanies.bind(null, e.target.value, startingEntry), SEARCH_TIMEOUT_DELAY);
+			searchTimestamp = setTimeout(
+				searchCompanies.bind(null, e.target.value, startingEntry, filterString), 
+				SEARCH_TIMEOUT_DELAY
+			);
 		});
-
+		// Detailed view event listener
 		companyList.addEventListener("click", function(e) {
 			if (e.target.innerHTML)
 				renderDetailedCompanyView(e.target.dataset);
 		});
+		// Filtering event listeners
+		filters.forEach((filter) => {
+			filter.addEventListener("change", function(e) {
+				const selectedFilters = [];
 
+				filters.forEach((f) => {
+					if (f.checked)
+						selectedFilters.push(f.value);
+				});
+
+				startingEntry = 0;
+				filterString = selectedFilters.join(',');
+				searchCompanies(search.value, startingEntry, filterString);
+			});
+		});
+		// Pagination event listeners
 		previousPage.addEventListener("click", function(e) {
 			if (startingEntry - 10 >= 0) {
 				startingEntry -= 10;
-				searchCompanies(search.value, startingEntry);
+				searchCompanies(search.value, startingEntry, filterString);
 			}
 		});
-
 		nextPage.addEventListener("click", function(e) {
 			if (startingEntry < totalEntries - 10) {
 				startingEntry += 10;
-				searchCompanies(search.value, startingEntry);
+				searchCompanies(search.value, startingEntry, filterString);
 			}
 		});
 	}
